faf8e1efbf9605d2af1affdb76350d773e9fb63f
Ankit Dalei <134222760+Ankit-Dalei@users.noreply.github.com>
2023-10-23 20:14:53 +0530

    initial commit

--- app/src/main/java/com/example/bhaitalks/loginActivity.java
+++ app/src/main/java/com/example/bhaitalks/loginActivity.java
@@ -1,14 +1,42 @@
 package com.example.bhaitalks;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class loginActivity extends AppCompatActivity {
-
+Button button3,button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        button3=(Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(loginActivity.this, "Please Provide UserName and Password"  , Toast.LENGTH_LONG).show();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frgm, logfrag.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
+        button4=(Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(loginActivity.this, "Please Provide Valid Details"  , Toast.LENGTH_LONG).show();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.frgm, signfrag.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("name") // Name can be null
+                        .commit();
+            }
+        });
     }
 }
