0d66fd8245adca83421625b1f90631780b2dd6e9
Ankit Dalei <134222760+Ankit-Dalei@users.noreply.github.com>
2023-11-15 03:04:14 +0530

    commit

--- app/src/main/java/com/example/bhaitalks/DBhelper.java
+++ app/src/main/java/com/example/bhaitalks/DBhelper.java
@@ -1,4 +1,43 @@
 package com.example.bhaitalks;
 
-public class DBhelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class DBhelper extends SQLiteOpenHelper {
+
+    public DBhelper(@Nullable Context context ) {
+        super(context, "bhaitalks.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create table users(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,EMAIL TEXT,PHONE INTEGER,PASSWORD TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int i1) {
+        DB.execSQL("DROP TABLE if EXISTS users");
+        onCreate(DB);
+    }
+
+    public boolean insertData(String name,String email,String phone,String password)
+    {
+        SQLiteDatabase DB=this.getReadableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(name, name);
+        contentValues.put(email, email);
+        contentValues.put(phone, phone);
+        contentValues.put(password, password);
+        long result=DB.insert("users",null ,contentValues);
+        if(result==-1)
+            return false;
+        else
+            return true;
+    }
 }
--- app/src/main/java/com/example/bhaitalks/SignIn_welcome.java
+++ app/src/main/java/com/example/bhaitalks/SignIn_welcome.java
@@ -2,21 +2,36 @@ package com.example.bhaitalks;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import androidx.appcompat.app.AlertDialog;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class SignIn_welcome extends AppCompatActivity {
     TextView tdt;
+    DBhelper myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_welcome);
-        tdt=(TextView) findViewById(R.id.textView4);
+        myDb=new DBhelper(this);
         String uname=getIntent().getStringExtra("uname");
         String Pass=getIntent().getStringExtra("Pass");
         String Email=getIntent().getStringExtra("Email");
         String phon=getIntent().getStringExtra("phon");
-        tdt.setText("Hello"+uname);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                boolean isInserted= myDb.insertData( uname, Email, phon, Pass);
+                if(isInserted==true)
+                    Toast.makeText(SignIn_welcome.this, "Data Inserted Successfully.", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(SignIn_welcome.this,"Failed Insertion.",Toast.LENGTH_LONG).show();
+            }
+        },3000);
 
     }
 }
